33ae25ddfe51a6f282789e287f2c9356f2654d9f
Filipepy <54276753+Filipepy@users.noreply.github.com>
2024-08-14 23:02:13 -0300

    Update __init__.py

--- HRrequest/__init__.py
+++ HRrequest/__init__.py
@@ -1,11 +1,14 @@
+import sqlalchemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'da0a3ca93705e3e8c580a3b6cde29fae'
+
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
@@ -17,5 +20,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from HRrequest import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de Dados criado")
+else:
+    print("Base de dados já existente")
 
 from HRrequest import routes
